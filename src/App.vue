a841ce5f8e314564ad3fd544bf8796d15d6b94e9
Aleksey <d_team-lex@bk.ru>
2023-05-18 18:02:12 +0300

    init

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="app">{{ text }}</div>
+</template>
+
+<script lang="ts" setup>
+import { ref } from 'vue';
+
+const text = ref('Hello');
+</script>
+
+<style lang="scss">
+</style>
